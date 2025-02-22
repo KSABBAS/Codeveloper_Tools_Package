8efb84574b327c7e6169bd63f39e89a8bbb6208b
KS_ABBAS <145643357+KSABBAS@users.noreply.github.com>
2025-02-22 19:46:24 +0200

    added exports

--- codeveloper_tools/lib/codeveloper_tools.dart
+++ codeveloper_tools/lib/codeveloper_tools.dart
@@ -1,5 +1,108 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// lib/codeveloper_tools.dart
+library codeveloper_tools;
+
+// --------------- Functions Folder ---------------
+
+// Launcher_Functions
+export 'MyTools/Functions/Launcher_Functions/LaunchURL.dart';
+
+// Media_Picker_Tools
+export 'MyTools/Functions/Media_Picker_Tools/File_Picker_Package/File_Picker_Package.dart';
+export 'MyTools/Functions/Media_Picker_Tools/Image_Picker_Package/Image_Picker_Package.dart';
+export 'MyTools/Functions/Height_and_width_Functions.dart';
+export 'MyTools/Functions/MyResponsive.dart';
+export 'MyTools/Functions/WidgetListBuilder.dart';
+
+// --------------- Tools Folder ---------------
+
+// App_Containing_Tools -> Navigation_Bars
+export 'MyTools/tools/App_Containing_Tools/Navigation_Bars/NavBar.dart';
+export 'MyTools/tools/App_Containing_Tools/Navigation_Bars/PageViewNavBar.dart';
+export 'MyTools/tools/App_Containing_Tools/Navigation_Bars/PopAndVanishLAyerBetweenNavBar.dart';
+export 'MyTools/tools/App_Containing_Tools/Navigation_Bars/PopAndVanishNavBar.dart';
+export 'MyTools/tools/App_Containing_Tools/ExpandableThrowableButton.dart';
+export 'MyTools/tools/App_Containing_Tools/SearchAppBar.dart';
+export 'MyTools/tools/App_Containing_Tools/StageIndicator.dart';
+export 'MyTools/tools/App_Containing_Tools/TBMaker.dart';
+
+// Audio_tools
+export 'MyTools/tools/Audio_tools/audio_recorder.dart';
+export 'MyTools/tools/Audio_tools/miniAudioPlayer.dart';
+
+// builder_tools
+export 'MyTools/tools/builder_tools/cardViewWidget.dart';
+export 'MyTools/tools/builder_tools/DistributiveGView.dart';
+export 'MyTools/tools/builder_tools/MyMiniOnTheRightCardViewer.dart';
+export 'MyTools/tools/builder_tools/MySwitchTitleBuilder.dart';
+export 'MyTools/tools/builder_tools/Specific_height_width_grid.dart';
+
+// Button_Tools
+export 'MyTools/tools/Button_Tools/Checkbox/MultiCBox.dart';
+export 'MyTools/tools/Button_Tools/DropDown/DDButton.dart';
+export 'MyTools/tools/Button_Tools/Radio/MultiRButton.dart';
+export 'MyTools/tools/Button_Tools/MyButton.dart';
+export 'MyTools/tools/Button_Tools/StarsRating.dart';
+
+// CMaker_Tools
+export 'MyTools/tools/CMaker_Tools/AnimatedCMaker.dart';
+export 'MyTools/tools/CMaker_Tools/CMaker.dart';
+
+// Generator_Tools
+export 'MyTools/tools/Generator_Tools/CustomToolTip.dart';
+export 'MyTools/tools/Generator_Tools/dayListGenerator.dart';
+export 'MyTools/tools/Generator_Tools/FlipCard.dart';
+export 'MyTools/tools/Generator_Tools/generateQRCode.dart';
+export 'MyTools/tools/Generator_Tools/generateStarRating.dart';
+export 'MyTools/tools/Generator_Tools/MyPopUpWidget.dart' hide PopupDirection;
+
+// Grids_Tools
+export 'MyTools/tools/Grids_Tools/WGrid.dart';
+
+// Image_Tools -> Image_Viewers
+export 'MyTools/tools/Image_Tools/Image_Viewers/ClickToOpenImageViwer.dart';
+export 'MyTools/tools/Image_Tools/Image_Viewers/Dotted_image_viewer.dart';
+export 'MyTools/tools/Image_Tools/Image_Viewers/miniOnTheRightImageView.dart';
+
+// Painter_tools
+export 'MyTools/tools/Painter_tools/PercentageCircle.dart';
+
+// Responsive_Tools
+export 'MyTools/tools/Responsive_Tools/ResponsivePMaker.dart';
+
+// Scanner_Tools
+export 'MyTools/tools/Scanner_Tools/scanQR.dart';
+
+// Selector_Tools
+export 'MyTools/tools/Selector_Tools/MyColumnWidgetSelector.dart';
+export 'MyTools/tools/Selector_Tools/MyExpandingColumnWidgetSelector.dart';
+export 'MyTools/tools/Selector_Tools/MyExpandingRowWidgetSelector.dart';
+export 'MyTools/tools/Selector_Tools/MyRowWidgetSelector.dart';
+
+// Spacing_And_Alignment_Tools
+export 'MyTools/tools/Spacing_ And_Alignment_Tools/CenterHorizontal.dart';
+export 'MyTools/tools/Spacing_ And_Alignment_Tools/CenterVertical.dart';
+export 'MyTools/tools/Spacing_ And_Alignment_Tools/PMaker.dart';
+
+// SplashView_Tools
+export 'MyTools/tools/SplashView_Tools/SplashViewPage.dart';
+
+// Switch_Tools
+export 'MyTools/tools/Switch_Tools/MySwitch.dart';
+
+// Text_Tools
+export 'MyTools/tools/Text_Tools/TMaker.dart';
+
+// TextField_Tools
+export 'MyTools/tools/TextField_Tools/TFFMaker.dart';
+export 'MyTools/tools/TextField_Tools/TFMaker.dart';
+
+// Timer_Tools
+export 'MyTools/tools/Timer_Tools/CircularCountdownTimer.dart';
+export 'MyTools/tools/Timer_Tools/FlipTimer.dart';
+export 'MyTools/tools/Timer_Tools/NowClock.dart';
+
+// Video_Tools
+export 'MyTools/tools/Video_Tools/Chewie_Player/ChewieVideoPlayer.dart';
+export 'MyTools/tools/Video_Tools/MediaKitVideoPlayer/MyMediaKitVideo.dart';
+export 'MyTools/tools/Video_Tools/miniVideoPlayer/miniVideoPlayer.dart';
+export 'MyTools/tools/Video_Tools/MyVideoPlayers/MyVideoPlayer.dart';
